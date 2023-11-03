a13e622bbfda1dfc20aeec6f67e9b58dbd806c16
Paulina Guzior <paulinkaguzior7@gmail.com>
2023-11-03 15:35:22 +0100

    add models

--- main/models.py
+++ main/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Mentor(models.Model):
+    mentor_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12)
 
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
@@ -8,6 +15,7 @@ class Course(models.Model):
     net_amount = models.DecimalField(max_digits=6, decimal_places=2)
     vat = models.DecimalField(max_digits=3, decimal_places=2)
     language_cd = models.CharField(max_length=2)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
 
 
 class User(models.Model):
@@ -58,3 +66,38 @@ class ExamQuestionAnswer(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
     correct = models.BooleanField()
+
+class ExamExamQuestion(models.Model):
+    exam_exam_question_id = models.AutoField(primary_key=True)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    exam_question = models.ForeignKey(ExamQuestion, on_delete=models.CASCADE)
+
+class Subject(models.Model):
+    subject_id = models.AutoField(primary_key=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    seqence = models.CharField(max_length=1000)
+
+class Lesson(models.Model):
+    lesson_id = models.AutoField(primary_key=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    seqence = models.CharField(max_length=1000, null=True, blank=True)
+    short_description = models.CharField(max_length=100, null=True, blank=True)
+    description = models.CharField(max_length=1000)
+
+class LessonContent(models.Model):
+    lesson_content_id = models.AutoField(primary_key=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    seqence = models.CharField(max_length=1000)
+    content_type = models.CharField(max_length=100)
+
+class Certificate(models.Model):
+    certificate_id = models.AutoField(primary_key=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_from = models.DateField()
+    date_to = models.DateField(null=True, blank=True)
+    certificate_pdf_path = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
