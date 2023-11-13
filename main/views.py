24ef05c3a5d913b48bfa1fe6ea442814ff41653c
benhus8 <benedykt.huszcza@gmail.com>
2023-11-13 17:15:07 +0100

    add login view in django

--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+@login_required()
 def index(request):
     return render(request, 'index.html')
+
+
+class MyLoginView(LoginView):
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
 
