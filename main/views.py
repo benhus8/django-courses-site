620b62ab6180b6e825d9a1fec6b132c836791d4f
benhus8 <benedykt.huszcza@gmail.com>
2023-11-13 20:05:09 +0100

    add redirecting from logout link to login page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -20,4 +20,7 @@ class MyLoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, 'Invalid username or password')
         return self.render_to_response(self.get_context_data(form=form))
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
 
