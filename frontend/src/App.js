1826a7fc63c3a1b0d4651cbc7fc8ed2868cf5b45
benhus8 <benedykt.huszcza@gmail.com>
2023-11-13 20:05:53 +0100

    extend App.js add App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Navbar from "./components/Navbar";
+import Account from "./views/Account";
+import {Route, Routes, Link} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="App">
+            {/*<Navbar/>*/}
+            <div>
+                    <Routes>
+                        <Route exact path="" element={<Navbar/>}/>
+                        <Route exact path="/account" element={<Account/>}/>
+                        <Route exact path="shop" element={<Account/>}/>
+                    </Routes>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
