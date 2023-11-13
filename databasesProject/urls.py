86e9ec616a301ab027b4a43a163fca21eef93d37
benhus8 <benedykt.huszcza@gmail.com>
2023-11-13 17:15:25 +0100

    include login nview in urls

--- databasesProject/urls.py
+++ databasesProject/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from main import views
+from main.views import MyLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', MyLoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
