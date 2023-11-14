7d347c2b59273e1f47448c44c23e58f53b98f9da
benhus8 <benedykt.huszcza@gmail.com>
2023-11-14 20:29:38 +0100

    extend urls

--- databasesProject/urls.py
+++ databasesProject/urls.py
@@ -15,16 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path, include
+from django.urls import path
 from main import views
 from main.views import MyLoginView, MyLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('account', views.index, name='index'),
+    path('account', views.index, name='account'),
+    path('shop', views.index, name='shop'),
+    path('register', views.registerView, name='registerView'),
+    path('my-courses', views.index, name='my-courses'),
     path('login/', MyLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
+    path('api/get_user_data/', views.get_user_data, name='get_user_data'),
+    path('api/register', views.register, name='register'),
 
 ]
