174feff010b7c5bf9f60010e9255803b78af22cb
benhus8 <benedykt.huszcza@gmail.com>
2023-11-14 20:13:28 +0100

    add forms.py

---
+++ main/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'birthday', 'phone_number',
+                  'address', 'building_number', 'postal_code', 'city']
