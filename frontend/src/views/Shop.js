66c9d84bfaa665ca6609f1411759dea3a7c7f3ff
benhus8 <benedykt.huszcza@gmail.com>
2023-11-14 20:15:01 +0100

    add shop view template

---
+++ frontend/src/views/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className="container mt-5">
+            <div className="card">
+                <div className="card-body">
+                    <p className="card-text">Shop View</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
