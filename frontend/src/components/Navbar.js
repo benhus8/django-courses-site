5e313995f4a8e8bbee64dbbbc6a3018d1879e23e
benhus8 <benedykt.huszcza@gmail.com>
2023-11-14 20:12:56 +0100

    stylize navbar menu with bootstrap

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import '../App.css';
 import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item"> <Link reloadDocument  to="/shop"> Sklep </Link> </li>
-        <li className="navbar-item"><Link reloadDocument  to="/my-courses"> Moje kursy </Link></li>
-        <li className="navbar-item"><Link reloadDocument  to="/account"> Moje konto </Link></li>
-        <li className="navbar-item"><Link reloadDocument to="/logout"> Wyloguj się</Link></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+      <div className="container">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link  reloadDocument className="nav-link text-white fs-4" to="/shop">Shop</Link>
+          </li>
+          <li className="nav-item">
+            <Link reloadDocument className="nav-link text-white fs-4" to="/my-courses">My Courses</Link>
+          </li>
+          <li className="nav-item">
+            <Link reloadDocument className="nav-link text-white fs-4" to="/account">My account</Link>
+          </li>
+          <li className="nav-item">
+            <Link reloadDocument className="nav-link text-white fs-4" to="/logout">Log out</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
