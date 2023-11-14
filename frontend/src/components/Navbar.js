a400f3f50db04e4f517f463081c131c199f6778a
benhus8 <benedykt.huszcza@gmail.com>
2023-11-14 23:52:38 +0100

    add hover to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+    <nav className="navbar navbar-expand-lg navbar-light bg-info justify-content-between">
       <div className="container">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link  reloadDocument className="nav-link text-white fs-4" to="/shop">Shop</Link>
+            <Link className="nav-link text-white fs-4" to="/shop">Shop</Link>
           </li>
           <li className="nav-item">
-            <Link reloadDocument className="nav-link text-white fs-4" to="/my-courses">My Courses</Link>
+            <Link className="nav-link text-white fs-4" to="/my-courses">My Courses</Link>
           </li>
           <li className="nav-item">
-            <Link reloadDocument className="nav-link text-white fs-4" to="/account">My account</Link>
+            <Link className="nav-link text-white fs-4" to="/account">My account</Link>
           </li>
           <li className="nav-item">
-            <Link reloadDocument className="nav-link text-white fs-4" to="/logout">Log out</Link>
+            <Link className="nav-link text-white fs-4" to="/logout">Log out</Link>
           </li>
         </ul>
       </div>
