4235dc0618286839bc68a739fc7012bc52833af2
benhus8 <benedykt.huszcza@gmail.com>
2023-11-15 22:14:55 +0100

    refactor navbar color and reloading

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,20 +4,20 @@ import './navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary justify-content-between">
       <div className="container">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link text-white fs-4" to="/shop">Shop</Link>
+            <Link  reloadDocument className="nav-link text-white fs-4" to="/shop">Shop</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white fs-4" to="/my-courses">My Courses</Link>
+            <Link reloadDocument className="nav-link text-white fs-4" to="/my-courses">My Courses</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white fs-4" to="/account">My account</Link>
+            <Link reloadDocument className="nav-link text-white fs-4" to="/account">My account</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white fs-4" to="/logout">Log out</Link>
+            <Link reloadDocument className="nav-link text-white fs-4" to="/logout">Log out</Link>
           </li>
         </ul>
       </div>
